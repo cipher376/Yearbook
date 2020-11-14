0c099832500676bbc310fee6bfa6d6d869f684cb
sammy-k3lly <xyphon5@gmail.com>
2020-11-14 18:40:49 +0000

    fixed profile cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,76 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { AuthGuardService } from './shared/services/providers/authentication/auth-guard.service';
-import { ScreenLockGuardService } from './shared/services/providers/authentication/screen-lock-guard.service';
-
-const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
-  
-  {
-    path: 'login',
-    loadChildren: './public/login/login.module#LoginPageModule'
-  },
-  {
-    path: 'register',
-    loadChildren: './public/register/register.module#RegisterPageModule'
-  },
-  {
-    path: 'forgot-password',
-    loadChildren: './public/forgot-password/forgot-password.module#ForgotPasswordPageModule'
-  },
-  {
-    path: 'tutorial',
-    loadChildren: './public/tutorial/tutorial.module#TutorialModule'
-  },
-  {
-    path: 'login-unauth',
-    loadChildren: () => import('./public/login-preview-un-auth/login-preview-un-auth.module').then( m => m.LoginPreviewUnAuthPageModule)
-  },
-  {
-    canActivate: [AuthGuardService, ScreenLockGuardService],
-    canActivateChild: [AuthGuardService],
-    path: 'login-auth',
-    loadChildren: () => import('./public/login-preview-auth/login-preview-auth.module').then( m => m.LoginPreviewAuthPageModule)
-  },
-  {
-    canActivate: [AuthGuardService, ScreenLockGuardService],
-    canActivateChild: [AuthGuardService],
-    path: 'phone-number',
-    loadChildren: () => import('./public/phone-number/phone-number.module').then( m => m.PhoneNumberPageModule)
-  },
-  // {
-  //   canActivate: [ScreenLockGuardService],
-  //   path: 'home',
-  //   loadChildren: () => import('./members/home/home.module').then( m => m.HomePageModule)
-  // },
-  // {   canActivate: [ScreenLockGuardService],
-  //   path: 'schools',
-  //   loadChildren: () => import('./members/schools/schools.module').then( m => m.SchoolsPageModule)
-  // },
-  // {
-  //   path: 'settings',
-  //   loadChildren: () => import('./members/settings/settings.module').then( m => m.SettingsPageModule)
-  // },
-  // {
-  //   canActivate: [AuthGuardService, ScreenLockGuardService],
-  //   canActivateChild: [AuthGuardService],
-  //   path: 'profile',
-  //   loadChildren: () => import('./members/profile/profile.module').then( m => m.ProfilePageModule)
-  // },
-  {
-    path: '',
-    loadChildren: () => import('./members/tab-links/tab-links.module').then( m => m.TabLinksPageModule)
-  },  {
-    path: 'comments',
-    loadChildren: () => import('./members/comments/comments.module').then( m => m.CommentsPageModule)
-  },
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -154,7 +81,6 @@ const routes: Routes = [
   //   loadChildren: () => import('./members/schools-profile/schools-profile.module').then( m => m.SchoolsProfilePageModule)
   // },
 ];
->>>>>>> master
 
  
   // {
@@ -169,7 +95,7 @@ const routes: Routes = [
   //   path: 'schools-profile',
   //   loadChildren: () => import('./members/schools-profile/schools-profile.module').then( m => m.SchoolsProfilePageModule)
   // },
-];
+// ];
 
 @NgModule({
   imports: [
