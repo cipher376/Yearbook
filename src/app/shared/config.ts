03a378f9e2093a37e238756a4cf8ccf6782f6514
Andy67K <andy67km@gmail.com>
2020-12-16 23:35:24 +0000

    'School profile, image-background change'

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -13,7 +13,7 @@ export const API_ROOT_URL = `http://devtek-limited.tech:${API_PORT || 80}`;
  */
 export const SCHOOL_DEFAULT_PHOTO_URL = `${API_ROOT_URL}/media/download/school_default.png`;
 export const NO_SCHOOL_COVER_PHOTO_URL = `${API_ROOT_URL}/media/download/school_default_cover.jpg`;
-export const USER_DEFAULT_PHOTO_URL = `${API_ROOT_URL}/media/download/user_default.png`;
+export const USER_DEFAULT_PHOTO_URL = `${API_ROOT_URL}/media/download/user_default.svg`;
 export const CREST_DEFAULT_PHOTO_URL = `${API_ROOT_URL}/media/download/crest_default.png`;
 export const USER_DEFAULT_COVER_URL = '';
 
